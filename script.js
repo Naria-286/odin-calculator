5b280c814b0bed6500f931ab2ad9f4c8cce969c5
Naria-286 <1885397556+Naria-286@users.noreply.github.com>
2024-12-27 20:05:46 -0700

    Add operate function

--- script.js
+++ script.js
@@ -10,3 +10,19 @@ function multiply (a,b) {
 function divide(a,b) {
     return a / b
 }
+let firstNumber
+let operator
+let secondNumber
+function operate (first, operator, second) {
+    switch (operator) {
+        case "+":
+            return add(first,second)
+        case "-":
+            return subtract(first,second)
+        case "*":
+            return multiply(first,second)
+        case "/":
+            return divide(first,second)
+    }
+
+}
